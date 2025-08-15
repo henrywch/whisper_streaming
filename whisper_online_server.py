80089243c43c1a4ef437c5ec85acfd8ac8718ec9
henrywch <2462977089@qq.com>
2025-08-15 23:54:16 +0800

    Live.

--- whisper_online_server.py
+++ whisper_online_server.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser()
 
 # server options
 parser.add_argument("--host", type=str, default='localhost')
-parser.add_argument("--port", type=int, default=43007)
+parser.add_argument("--port", type=int, default=43001)
 parser.add_argument("--warmup-file", type=str, dest="warmup_file", 
         help="The path to a speech audio wav file to warm up Whisper so that the very first chunk processing is fast. It can be e.g. https://github.com/ggerganov/whisper.cpp/raw/master/samples/jfk.wav .")
 
