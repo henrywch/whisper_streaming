c6cfb83aff9d2a49e5660fac977e519607529f44
henrywch <2462977089@qq.com>
2025-08-19 19:36:57 +0800

    It can talk now?

--- whisper_online_server.py
+++ whisper_online_server.py
@@ -103,7 +103,7 @@ class ServerProcessor:
         self.is_first = True
 
         self.new_session = True
-        self.current_time = str()
+        self.outf_name = str()
 
     def receive_audio_chunk(self):
         # receive all audio that is available by this time
@@ -149,9 +149,11 @@ class ServerProcessor:
             print(format_line ,flush=True,file=sys.stderr)
 
             if self.new_session:
-                self.current_time= datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-            with open(f"results/LIVE_{self.current_time}.txt", "a") as f:
-                f.write(format_line + "\n")
+                self.outf_name= "results\LIVE_{}.txt".format(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+                self.new_session = False
+            with open(self.outf_name, mode="a") as f:
+                f.write("%s " % o[2])
+                f.flush()
 
             return format_line
         else:
@@ -182,6 +184,119 @@ class ServerProcessor:
 #        self.send_result(o)
 
 
+######### The Talking Codes
+
+import csv
+import glob
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from kokoro import KPipeline
+import sounddevice as sd
+
+class MockingBird:
+
+    def __init__(self, model_path):
+        self.model_path = model_path
+        self.load_llm()
+        self.model = None
+        self.tokenizer = None
+
+        self.ans_folder = "results\ANS_{}".format(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+        self.ans_file_name = os.path.join(self.ans_folder, "chat_history.tsv")
+
+    def load_llm(self):
+        self.model = AutoModelForCausalLM.from_pretrained(
+            self.model_path,
+            torch_dtype="auto",
+            device_map="auto"
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_path)
+
+    def locate_dialogs(self, local_dir: str = "results"):
+        search_pattern = os.path.join(local_dir, 'LIVE_*.txt')
+        matching_files = glob.glob(search_pattern)
+
+        if not matching_files:
+            return None
+        last_file_path = sorted(matching_files)[-1]
+
+        return os.path.basename(last_file_path)
+
+    def load_chat_history(self):
+        history = []
+        if os.path.exists(self.ans_file_name) and os.stat(self.ans_file_name).st_size > 0:
+            with open(self.ans_file_name, "r", newline='', encoding='utf-8') as f:
+                reader = csv.DictReader(f, delimiter='\t')
+                for row in reader:
+                    history.append({"role": "user", "content": row['user']})
+                    history.append({"role": "assistant", "content": row['bot']})
+        return history
+
+    def llm_thinking(self):
+        dialog_file_name = self.locate_dialogs()
+
+        if not dialog_file_name:
+            print("No new dialog file found. Exiting.")
+            return
+
+        with open(dialog_file_name, "r") as f:
+            dialogs = f.read().strip()
+
+        messages = self.load_chat_history()
+
+        messages.insert(0, {"role": "system", "content": """
+                                          You are Qwen, created by Alibaba Cloud. You are a helpful assistant. \
+                                          Now someone likes to talk you, he/she can be anybody. Just calm down \
+                                          and talk to him/her according to what you are trained. Don't let them \
+                                          find out that YOU are a ROBOT.
+                                          The consequence can be DISASTROUS!!!"""
+                            })
+        messages.append({"role": "user", "content": dialogs})
+
+        text = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+        model_inputs = self.tokenizer([text], return_tensors="pt").to(self.model.device)
+
+        generated_ids = self.model.generate(
+            **model_inputs,
+            max_new_tokens=512
+        )
+        generated_ids = [
+            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+        ]
+
+        response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+        file_exists = os.path.exists(self.ans_file_name)
+
+        with open(self.ans_file_name, "a", newline='', encoding='utf-8') as f:
+            fieldnames = ['user', 'bot']
+            writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter='\t')
+
+            if not file_exists or os.stat(self.ans_file_name).st_size == 0:
+                writer.writeheader()
+
+            writer.writerow({'user': dialogs, 'bot': response})
+
+        self.kokoro_output(response)
+
+    def kokoro_output(self, response: str = "Sorry, pardon me?"):
+        if not os.path.exists(self.ans_folder):
+            os.makedirs(self.ans_folder)
+
+        pipeline = KPipeline(lang_code='b')
+        generator = pipeline(response, voice='af_heart')
+
+        for i, (gs, ps, audio) in enumerate(generator):
+            print(f"Generating part {i}: {gs}, {ps}")
+            sd.play(audio, samplerate=24000)
+            sd.wait()
+
+            sf.write(os.path.join(self.ans_folder, f'{i}.wav'), audio, 24000)
+
 # server loop
 def server_loop(se):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -199,7 +314,10 @@ def server_loop(se):
 
     logger.info('Connection closed, terminating.')
 
+
 if __name__ == '__main__':
+    mockbd = MockingBird(".\models\Qwen")
+
     stop_event = threading.Event()
     server_thread = threading.Thread(target=server_loop, args=(stop_event,))
     server_thread.daemon = True
